1f464e6e0ef118498dfe8e6d8899fe136b2b6b80
eliada99 <eliada0525>
2019-12-18 13:45:58 +0200

    check the option commit via pycharm

--- main.py
+++ main.py
@@ -57,6 +57,8 @@ if __name__ == '__main__':
         usage()
 
     globals.init()  # create relevant objects and save them in globals module
+
+
     #  WIP - under my debug:
     tests.update_setup_bluefield(globals.hostLinuxServer)
     tests.update_setup_bluefield(globals.hostLinuxClient)
