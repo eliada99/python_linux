d0b49220598789bf182f795f607042157462cc24
eliada99 <eliada0525>
2019-12-30 17:04:18 +0200

    add new test - ping in ipv4 and ipv6

--- tests/tests.py
+++ tests/tests.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python
+
 from modules import hostGetData
 import globals
+from modules import utilities
 
 VERSIONS_PATH = "../versions.txt"
 
 
-def update_setup_bluefield(remoteObj):
+def update_setup_bluefield(Obj):
     myRemoteLocal = globals.hostLinuxServer
     myRemoteLocal = globals.hostLinuxClient
     verDic = hostGetData.get_versions_from_file(VERSIONS_PATH)
     pass
     return
     #remoteObj.run_cmd()
+
+
+# client = Host object
+# serverInt = HostInterfaceEth object
+# clientInt = HostInterfaceEth object
+# ipv = string of "ipv4" or "ipv6"
+def run_ping(client, server_int, client_nt, ipv):
+    if ipv is "ipv4":
+        server_ip = server_int.get_ipv4()
+        ping = "ping"
+    elif ipv is "ipv6":
+        server_ip = server_int.get_ipv6()
+        ping = "ping6"
+
+    cmd = ping + " -c 3 -I " + client_nt.get_interface_name() + " " + server_ip
+    try:
+        return client.run_cmd(cmd, 0, 1)
+    except:
+        utilities.reporter("Ping fail: " + cmd + "\n", 'red')
+        return None
