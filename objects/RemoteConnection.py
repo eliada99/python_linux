b7a00117df36dacf39f1164ac051c7bf04214e64
eliada <eliadush9@gmail.com>
2019-12-31 15:51:16 +0200

    fix the bug with my recursion...

--- objects/RemoteConnection.py
+++ objects/RemoteConnection.py
@@ -6,28 +6,30 @@ from modules import sshParamiko, utilities
 import socket
 
 VER_TOOLS_PATH = '/mswg/projects/ver_tools/reg2_latest/install.sh'
-recCounter = 0
 
 
 class RemoteConnection(object):
-    recCounter = 0
     pass
 
+    def __init__(self):
+        self.recCounter = 0
+
     def connect(self, ip):
-        if RemoteConnection.recCounter == 2:
+        if self.recCounter == 2:
             utilities.reporter("No connection to host: " + ip + "!", 'red')
             return None
         try:
             conn = rpyc.classic.connect(ip)
             return conn
         except Exception as err:
-            ++RemoteConnection.recCounter
-            utilities.reporter("No connection to host: " + ip + " via RPyC service, starting to install the ver_tools: "
-                               + VER_TOOLS_PATH, 'yellow')
+            ++self.recCounter
+            utilities.reporter("No connection to host: " + ip + " via RPyC service.\n" +
+                               "starting to install the ver_tools: " + VER_TOOLS_PATH, 'yellow')
             sshParamiko.ssh_command(str(ip), VER_TOOLS_PATH)
             return self.connect(ip)  # recursion, it is a good day to die
 
     def connect_to_arm(self, ip):
+        pass
         '''
         The problem with bluefield connection is that you dont have access to /mswg/ and you cant install
         # nothing via 'yum install' due to repos issue.
