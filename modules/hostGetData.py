991a0c3b3b08f8612ff2b5ba9cfcdd76c6cd8cc0
eliada <eliadush9@gmail.com>
2020-01-02 17:33:50 +0200

    change names according the json

--- modules/hostGetData.py
+++ modules/hostGetData.py
@@ -8,8 +8,8 @@ def get_versions_from_json():
     with open(JSON_FILE) as f:
         json_file = json.load(f)
         f.close()
-    dic_versions = {"bfbImage": json_file["update_setup"]["versions"]["BFB"],
-                    "driver": json_file["update_setup"]["versions"]["ofed_install_command"],
-                    "fw": json_file["update_setup"]["versions"]["FW"]}
+    dic_versions = {"BFB": json_file["update_setup"]["versions"]["BFB"],
+                    "ofed_install_command": json_file["update_setup"]["versions"]["ofed_install_command"],
+                    "FW": json_file["update_setup"]["versions"]["FW"]}
 
     return dic_versions
