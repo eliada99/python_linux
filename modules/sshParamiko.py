f7c12f99e28862959f504cca7bb6808e716acd6f
eliada99 <eliada0525>
2020-02-16 16:27:54 +0200

    pep8

--- modules/sshParamiko.py
+++ modules/sshParamiko.py
@@ -7,14 +7,14 @@ def ssh_command(ip, command):
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try :
+    try:
         client.connect(str(ip), port=22, username="root", password="3tango")
-    except :
+    except:
         ip, "error : ssh connection failed"
         return ip, "error : ssh connection failed"
     stdin, stdout, stderr = client.exec_command(str(command))
     stdout = stdout.read()
     stderr = stderr.read()
     client.close()
-    if stderr: return ip,"error : %s" % stderr
+    if stderr: return ip, "error : %s" % stderr
     else: return ip, stdout
